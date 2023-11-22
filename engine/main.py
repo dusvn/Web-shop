30bb53f03f55821f8320a4e24d843d8f82f1a166
Dusan Paripovic <paripovicdusanftn@gmail.com>
2023-11-22 13:53:32 +0100

    setup project

--- engine/main.py
+++ engine/main.py
@@ -1,21 +1,63 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from config import firebase_app, db
 from firebase_admin import firestore
+from Model.Korisnik import *
+
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"], methods=["GET", "POST", "PUT"])
 
-@app.route("/")
+
+@app.route("/api")
 def main():
-    print("Hello from the server!")
-    return "Welcome to the Flask App!"
+    return "Welcome!"
 
-@app.route("/proizvodi", methods=['GET'])
+
+@app.route("/api/proizvodi", methods=['GET'])
 def get_proizvodi():
-    data = []
+    data = dict()
     proizvodi = db.collection("Proizvodi").get()
     for proizvod in proizvodi:
-        data.append(f"{proizvod.id} => {proizvod.to_dict()}")
+        data[proizvod.id] = proizvod.to_dict()
     return jsonify(data)
 
+
+@app.route("/api/korisnici", methods=['GET'])
+def get_korisnici():
+    data = dict()
+    korisnici = db.collection("Korisnici").get()
+    for korisnik in korisnici:
+        data[korisnik.id] = korisnik.to_dict()
+    return jsonify(data)
+
+
+@app.route("/api/korisnik", methods=['GET'])
+def get_korisnik():
+    korisnik_parameter = request.args.get('email')
+    korisnik = db.collection("Korisnici").document(korisnik_parameter)
+    korisnik_snepsot = korisnik.get()
+    korisnik_objekat = korisnik_snepsot.to_dict()
+    if type(korisnik_objekat) is dict:
+        return jsonify(korisnik_objekat)
+    else:
+        return "Kita bato", 404
+
+
+@app.route("/api/korisnici", methods=['POST'])
+def napravi_korisnika():
+    newKorisnik = KorisnikSchema().load(request.get_json())
+    korisnik = db.collection("Korisnici").document(newKorisnik.email)
+    korisnik_snepsot = korisnik.get()
+    korisnik_objekat = korisnik_snepsot.to_dict()
+    if type(korisnik_objekat) is dict:
+        return "User already exists", 400
+    else:
+        data = {"Adresa": newKorisnik.adresa, "Broj telefona": newKorisnik.brtelefona, "Država": newKorisnik.drzava,
+                "Grad": newKorisnik.grad, "Ime": newKorisnik.ime, "Lozinka": newKorisnik.lozinka}
+        db.collection('Korisnici').document(newKorisnik.email).set(data)
+        return "User successfully created", 204
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
