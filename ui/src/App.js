e7d85828de9e5e1d171f78eea3b5ef956ddcb0f4
Dusan Paripovic <paripovicdusanftn@gmail.com>
2023-11-23 19:08:32 +0100

    dodat front za login i tailwind css

---
+++ ui/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+import {API_BASE_URL} from "./index";
+import Login  from "./components/Login";
+
+function App() {
+    return (
+        <Login/>
+    )
+}
+
+export default App;
+
---
+++ ui/src/components/Login.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+export default function Login() {
+    return(
+        <div className='grid grid-cols-1 sm:grid-cols-1 h-screen w-full'>
+            <div className='bg-gray-800 flex flex-col justify-center'>
+                <form className='max-w-[500px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
+                    <h2 className='text-4xl dark:text-white  text-center italic font-light'>LOGIN</h2>
+                    
+                    <div className='flex flex-col text-gray-400 py-2'> 
+                        <label>Email</label>
+                        <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus-outline-none' type='email' placeholder='email@gmail.com'></input>
+                    </div>
+
+                    <div className='flex flex-col text-gray-400 py-2'>
+                        <label>Password</label>
+                        <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus-outline-none' type='password' placeholder='***********'></input>
+                    </div>
+                    <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/40 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>LOGIN</button>
+                    
+                </form>
+            </div>
+        </div>
+        
+    )
+
+  
+}
+
