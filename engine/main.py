181f730d098130871ce45b8835d6bae237e9b60d
Dusan Paripovic <dusanparipovicftn@gmail.com>
2023-12-06 22:26:12 +0100

    log

--- engine/main.py
+++ engine/main.py
@@ -1,51 +1,59 @@
+# from functools import wraps
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from config import firebase_app, db
+from config import firebase_app, db, admin_ids
+from additional_functions import hash_pass
 from firebase_admin import firestore
 from Model.User import *
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt
 import secrets
-from flask.views import MethodView
-from webargs import fields, validate
-from webargs.flaskparser import use_args
-from passlib.hash import pbkdf2_sha256
+import hashlib
+from google.cloud.firestore_v1.base_query import FieldFilter  # nije potrebno ali neka stoji jer moze da zatreba taj fieldFilter pri queryjovanju iz baze
+# from flask.views import MethodView
+# from webargs import fields, validate
+# from webargs.flaskparser import use_args
+# from passlib.hash import pbkdf2_sha256
 
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"], methods=["GET", "POST", "PUT"])
+CORS(app, origins="*", methods=["GET", "POST", "PUT"])
 
-app.config["JWT_SECRET_KEY"] = secrets.SystemRandom().getrandbits(128) # svaki put kad se resetuje app imacemo drugi
-
+app.config["JWT_SECRET_KEY"] = "tajniKljuc"  # f"{secrets.SystemRandom().getrandbits(128)}"  # svaki put kad se resetuje app imacemo drugi
 jwt = JWTManager(app)
 
 
+
 @app.route('/api/register', methods=['POST'])
-@use_args(UserSchema(), location="json")
-def UserRegister(args): # ovaj metod view je da ako nam treba npr vise operacija tipa get delete put za isti obj
-    new_user = User(**args)
-
-    name = new_user._name
-    last_name = new_user._lastName
-    address = new_user._address
-    city = new_user._city
-    country = new_user._country
-    phone_num = new_user._phoneNum
-    email = new_user._email # ne moze na isti email
-    password = new_user._password
+def register_user():  # ovaj metod view je da ako nam treba npr vise operacija tipa get delete put za isti obj
+    new_user = UserSchema().load(request.get_json())
     # Check if the email is already taken
-    if is_email_taken(email):
+    if is_email_taken(new_user.email):
         return jsonify({"error": "Email is already taken"}), 400
-
+    new_user.password = hash_pass(new_user.password)
+    db.collection("Users").add(new_user.__dict__)  # ovo vraca nesto, probaj odatle da izvuces id i da ga zapises u bazu
     return jsonify({"message": "User registered successfully"}), 201
 
 
+@app.route('/api/login', methods=['POST'])
+def login_user():
+    new_user = UserLoginSchema().load(request.get_json())
+    if is_email_taken(new_user.email):
+        user_ref = db.collection("Users").where("email", "==", new_user.email).limit(1)
+        users = user_ref.stream()
+        for user in users:
+            user_data = user.to_dict()
+            if user_data['password'] == hash_pass(new_user.password):
+                access_token = create_access_token(identity=user.id)
+                return jsonify({"access_token": access_token}), 200
+            break
+    return {"message": "Invalid credentials"}, 400
+
+
 def is_email_taken(email):
-    # Get a reference to the "users" collection in your Firebase database
-    users_ref = db.reference("Users")
-
     # Query the database to check if a user with the specified email exists
-    result = users_ref.order_by_child('email').equal_to(email).get()
-
+    result = db.collection("Users").where("email", "==", email).get()
+    # print(result)
     # Check if the email is taken
     return bool(result)
 
@@ -62,14 +70,18 @@ def test_is_email_taken():
     print(f"Is '{email_taken}' taken? {result_taken}")
 
 
-
 @app.route("/api")
 def main():
     return "Welcome!"
 
 
 @app.route("/api/proizvodi", methods=['GET'])
+@jwt_required()
 def get_proizvodi():
+    jwt_token = get_jwt()
+    # print(jwt) jer se identity cuva u SUB polju a ne u IDENTITY kako smo ranije specificirali, super je ovaj pajton nema sta
+    if jwt_token.get("sub") not in admin_ids:
+        return {"message": "Unauthorized access"}, 400
     data = dict()
     proizvodi = db.collection("Proizvodi").get()
     for proizvod in proizvodi:
@@ -77,43 +89,22 @@ def get_proizvodi():
     return jsonify(data)
 
 
-@app.route("/api/korisnici", methods=['GET'])
-def get_korisnici():
+@app.route("/api/getUserName",methods=['GET'])
+@jwt_required()
+def getUserName():
+    jwt_token = get_jwt().get("sub") #ovo je zapravo user id
     data = dict()
-    korisnici = db.collection("Korisnici").get()
-    for korisnik in korisnici:
-        data[korisnik.id] = korisnik.to_dict()
-    return jsonify(data)
+    users = db.collection("Users").get()
+    for user in users:
+        if user.id == jwt_token:
+            data[user.id] = user.to_dict()
+            return jsonify(data[jwt_token]["name"]),200
+    return {"message": "Unauthorized access"}, 400
 
 
-@app.route("/api/korisnik", methods=['GET'])
-def get_korisnik():
-    korisnik_parameter = request.args.get('email')
-    korisnik = db.collection("Korisnici").document(korisnik_parameter)
-    korisnik_snepsot = korisnik.get()
-    korisnik_objekat = korisnik_snepsot.to_dict()
-    if type(korisnik_objekat) is dict:
-        return jsonify(korisnik_objekat)
-    else:
-        return "Kita bato", 404
 
-""""
-@app.route("/api/korisnici", methods=['POST'])
-def napravi_korisnika():
-    newKorisnik = KorisnikSchema().load(request.get_json())
-    korisnik = db.collection("Korisnici").document(newKorisnik.email)
-    korisnik_snepsot = korisnik.get()
-    korisnik_objekat = korisnik_snepsot.to_dict()
-    if type(korisnik_objekat) is dict:
-        return "User already exists", 400
-    else:
-        data = {"Adresa": newKorisnik.adresa, "Broj telefona": newKorisnik.brtelefona, "Država": newKorisnik.drzava,
-                "Grad": newKorisnik.grad, "Ime": newKorisnik.ime, "Lozinka": newKorisnik.lozinka}
-        db.collection('Korisnici').document(newKorisnik.email).set(data)
-        return "User successfully created", 204
-"""
+
 
 if __name__ == "__main__":
-    #test_is_email_taken()
+
     app.run()
-    #test_is_email_taken()
