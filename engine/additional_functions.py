ebfcd04038b3c3e69251d37e8c803c1b5a50b804
Dusan Paripovic <paripovicdusanftn@gmail.com>
2023-12-11 00:59:58 +0100

    git ...

--- engine/additional_functions.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def hash_pass(password):
-    hasher = hashlib.sha256(password.encode())
-    hashed = hasher.hexdigest()
-    return hashed
-
--- engine/config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import firestore, credentials
-
-# Application Default credentials are automatically created.
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-admin_ids = ["jDuJiPr3D8RGoOOhvVUi"]
